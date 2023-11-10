a6291f51df8c1ddb4b9f23de32ce5736fbc8ac06
Adam Stehlik <adam@cookielab.io>
2023-11-10 13:32:22 +0100

    Initial Commit

---
+++ xcode-cloud-test/xcode_cloud_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloud_testApp.swift
+//  xcode-cloud-test
+//
+//  Created by Adam Stehlik on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct xcode_cloud_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
